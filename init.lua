b0aefbe87acd69b3f823c0252d06918a2f033886
sevenwhiteclouds <maldonadokeldin@gmail.com>
2024-11-01 07:00:26 -0700

    add tab to next placeholder luasnippet

--- init.lua
+++ init.lua
@@ -180,17 +180,38 @@ require("mason-lspconfig").setup({
 
 -- to disable autocomplete: completion = {autocomplete = false}
 local cmp = require("cmp")
+local luasnip = require("luasnip")
+
 cmp.setup({
   sources = {
     {name = "nvim_lsp"},
+    {name = "luasnip"}
   },
+
   mapping = cmp.mapping.preset.insert({
     ["<CR>"] = cmp.mapping.confirm({select = true}),
     ["<C-Space>"] = cmp.mapping.complete(),
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, {"i", "s"}),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, {"i", "s"})
   }),
+
   snippet = {
     expand = function(args)
-      require("luasnip").lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end,
   },
 })
