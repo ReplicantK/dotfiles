77d947b47c992ce3e861c893aaf9c43e9d6a63a4
ReplicantK <maldonadokeldin@gmail.com>
2025-04-15 02:20:43 -0700

    improve setup script

--- setup.sh
+++ setup.sh
@@ -1,88 +1,85 @@
 #!/bin/sh
 
 # TODO: add try blocks and fail checks to this script; add set -xe?
-echo "Checking for .config directory... \c"
-if [ ! -d "$HOME/.config/" ]
+echo "Checking for config directory... \c"
+if [ ! -d "$HOME/.config" ]
 then
-  mkdir "$HOME/.config/"
-  echo "done -> created missing .config directory in home."
+  mkdir "$HOME/.config"
+  echo "done -> created missing $HOME/.config"
 else
-  echo "found .config directory in home."
+  echo "found $HOME/.config"
 fi
 
-echo "Checking for .vim directory... \c"
-if [ ! -d "$HOME/.vim/" ]
+echo "Checking for vim directory... \c"
+if [ ! -d "$HOME/.vim" ]
 then
-  mkdir "$HOME/.vim/"
-  echo "done -> created missing .vim directory in home."
+  mkdir "$HOME/.vim"
+  echo "done -> created missing $HOME/.vim"
 else
-  echo "found .vim directory in home."
+  echo "found $HOME/.vim"
 fi
 
 echo "Checking for mpv directory... \c"
-if [ ! -d "$HOME/.config/mpv/" ]
+if [ ! -d "$HOME/.config/mpv" ]
 then
   mkdir "$HOME/.config/mpv"
-  echo "done -> created missing mpv directory in .config"
+  echo "done -> created missing $HOME/.config/mpv"
 else
-  echo "found mpv directory in .config."
+  echo "found $HOME/.config/mpv"
 fi
 
 echo "Checking for nvim directory... \c"
-if [ ! -d "$HOME/.config/nvim/" ]
+if [ ! -d "$HOME/.config/nvim" ]
 then
   mkdir "$HOME/.config/nvim"
-  echo "done -> created missing nvim directory in .config"
+  echo "done -> created missing $HOME/.config/nvim"
 else
-  echo "found nvim directory in .config."
+  echo "found $HOME/.config/nvim"
 fi
 
 echo "Checking for zathura directory... \c"
-if [ ! -d "$HOME/.config/zathura/" ]
+if [ ! -d "$HOME/.config/zathura" ]
 then
-  mkdir "$HOME/.config/zathura/"
-  echo "done -> created missing zathura directory in .config"
+  mkdir "$HOME/.config/zathura"
+  echo "done -> created missing $HOME/.config/zathura"
 else
-  echo "found zathura directory in .config."
+  echo "found $HOME/.config/zathura"
 fi
 
 echo "Checking for kitty directory... \c"
-if [ ! -d "$HOME/.config/kitty/" ]
+if [ ! -d "$HOME/.config/kitty" ]
 then
-  mkdir "$HOME/.config/kitty/"
-  echo "done -> created missing kitty directory in .config"
+  mkdir "$HOME/.config/kitty"
+  echo "done -> created missing $HOME/.config/kitty"
 else
-  echo "found kitty directory in .config."
+  echo "found $HOME/.config/kitty"
 fi
 
 echo "Checking for alacritty directory... \c"
-if [ ! -d "$HOME/.config/alacritty/" ]
+if [ ! -d "$HOME/.config/alacritty" ]
 then
-  mkdir "$HOME/.config/alacritty/"
-  echo "done -> created missing alacritty directory in .config"
+  mkdir "$HOME/.config/alacritty"
+  echo "done -> created missing $HOME/.config/alacritty"
 else
-  echo "found alacritty directory in .config."
+  echo "found $HOME/.config/alacritty"
 fi
 
 echo "Checking for clangd directory... \c"
 
 clangd_dir=""
-clangd_dir_top=""
 if [ $(echo "$OSTYPE" | grep -c 'darwin') -gt 0 ]
 then
   clangd_dir="$HOME/Library/Preferences/clangd"
-  clangd_dir_top="~/Library"
 else
   clangd_dir="$HOME/.config/clangd"
-  clangd_dir_top="~/.config"
 fi
 
 if [ ! -d $clangd_dir ]
 then
   mkdir $clangd_dir
-  echo "done -> created missing clangd directory in $clangd_dir_top"
+  echo "done -> created missing $clangd_dir"
 else
-  echo "found clangd directory in $clangd_dir_top."
+  echo "found $clangd_dir"
 fi
 
 echo "Creating symbolic links... \c"
@@ -97,5 +94,5 @@ ln -sf "$HOME/dotfiles/vimrc" "$HOME/.vimrc"
 ln -sf "$HOME/dotfiles/vimrc" "$HOME/.ideavimrc"
 ln -sf "$HOME/dotfiles/zshrc" "$HOME/.zshrc"
 ln -sf "$HOME/dotfiles/config.yaml" "$clangd_dir/config.yaml"
-echo "done."
-echo "\nTask completed successfuly!"
+echo "done"
+echo "Setup completed successfuly!"
