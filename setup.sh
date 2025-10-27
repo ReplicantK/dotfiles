630fc1a2c1db19d9ab5afa31572f97ba46a6a818
ReplicantK <maldonadokeldin@gmail.com>
2025-10-27 05:59:28 -0700

    standardize around bash and simplify script

--- setup.sh
+++ setup.sh
@@ -1,88 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
 # TODO: add try blocks and fail checks to this script; add set -xe?
-echo "Checking for config directory... \c"
-if [ ! -d "$HOME/.config" ]
-then
-  mkdir "$HOME/.config"
-  echo "done -> created missing $HOME/.config"
-else
-  echo "found $HOME/.config"
-fi
-
-echo "Checking for vim directory... \c"
-if [ ! -d "$HOME/.vim" ]
-then
-  mkdir "$HOME/.vim"
-  echo "done -> created missing $HOME/.vim"
-else
-  echo "found $HOME/.vim"
-fi
-
-echo "Checking for mpv directory... \c"
-if [ ! -d "$HOME/.config/mpv" ]
-then
-  mkdir "$HOME/.config/mpv"
-  echo "done -> created missing $HOME/.config/mpv"
-else
-  echo "found $HOME/.config/mpv"
-fi
-
-echo "Checking for nvim directory... \c"
-if [ ! -d "$HOME/.config/nvim" ]
-then
-  mkdir "$HOME/.config/nvim"
-  echo "done -> created missing $HOME/.config/nvim"
-else
-  echo "found $HOME/.config/nvim"
-fi
-
-echo "Checking for zathura directory... \c"
-if [ ! -d "$HOME/.config/zathura" ]
-then
-  mkdir "$HOME/.config/zathura"
-  echo "done -> created missing $HOME/.config/zathura"
-else
-  echo "found $HOME/.config/zathura"
-fi
-
-echo "Checking for kitty directory... \c"
-if [ ! -d "$HOME/.config/kitty" ]
-then
-  mkdir "$HOME/.config/kitty"
-  echo "done -> created missing $HOME/.config/kitty"
-else
-  echo "found $HOME/.config/kitty"
-fi
-
-echo "Checking for alacritty directory... \c"
-if [ ! -d "$HOME/.config/alacritty" ]
-then
-  mkdir "$HOME/.config/alacritty"
-  echo "done -> created missing $HOME/.config/alacritty"
-else
-  echo "found $HOME/.config/alacritty"
-fi
-
-echo "Checking for clangd directory... \c"
+echo "Setup script launched."
+echo -n "Detecting operating system... "
 
 clangd_dir=""
-if [ $(echo "$OSTYPE" | grep -c 'darwin') -gt 0 ]
+
+if [[ "$OSTYPE" == darwin* ]]
 then
   clangd_dir="$HOME/Library/Preferences/clangd"
+  echo "macOS detected"
 else
   clangd_dir="$HOME/.config/clangd"
+  echo "GNU/Linux detected"
 fi
 
-if [ ! -d $clangd_dir ]
-then
-  mkdir $clangd_dir
-  echo "done -> created missing $clangd_dir"
-else
-  echo "found $clangd_dir"
-fi
+echo -n "Checking for required directories and creating missing ones... "
+mkdir -p "$clangd_dir"
+mkdir -p "$HOME/.config"
+mkdir -p "$HOME/.vim"
+mkdir -p "$HOME/.config/mpv"
+mkdir -p "$HOME/.config/nvim"
+mkdir -p "$HOME/.config/zathura"
+mkdir -p "$HOME/.config/kitty"
+mkdir -p "$HOME/.config/alacritty"
 
-echo "Creating symbolic links... \c"
+echo "done"
+
+echo -n "Creating symbolic links... "
 ln -sf "$HOME/dotfiles/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
 ln -sf "$HOME/dotfiles/mpv.conf" "$HOME/.config/mpv/mpv.conf"
 ln -sf "$HOME/dotfiles/init.lua" "$HOME/.config/nvim/init.lua"
@@ -94,5 +39,6 @@ ln -sf "$HOME/dotfiles/vimrc" "$HOME/.vimrc"
 ln -sf "$HOME/dotfiles/vimrc" "$HOME/.ideavimrc"
 ln -sf "$HOME/dotfiles/zshrc" "$HOME/.zshrc"
 ln -sf "$HOME/dotfiles/config.yaml" "$clangd_dir/config.yaml"
+
 echo "done"
-echo "Setup completed successfuly!"
+echo "Setup completed successfully!"
